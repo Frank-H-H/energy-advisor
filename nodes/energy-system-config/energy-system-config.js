72f073c0f28fad1256f63fffaa52b0bf8b42817c
DESKTOP-CFT3LCM\Frank <git.frank@huxol.one>
2026-08-28 00:14:51 +0200

    trying to get other config nodes running

--- nodes/energy-system-config/energy-system-config.js
+++ nodes/energy-system-config/energy-system-config.js
@@ -2,8 +2,8 @@ module.exports = function (RED) {
   function EnergySystemConfigNode(config) {
     RED.nodes.createNode(this, config)
     this.name = config.name
-    this.battery_ref = config.battery_ref || null
-    this.grid_ref = config.grid_ref || null
+    this.battery_config = config.battery_config || null
+    this.grid_config = config.grid_config || null
     this.analysis_interval_minutes = Number(config.analysis_interval_minutes ?? 15)
   }
   RED.nodes.registerType('energy-system-config', EnergySystemConfigNode)
