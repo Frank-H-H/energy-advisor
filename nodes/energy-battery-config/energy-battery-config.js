5cc46c6dd7a41bff09c2413fdcbf39ddc1539ed4
DESKTOP-CFT3LCM\Frank <git.frank@huxol.one>
2026-08-28 23:47:27 +0200

    added discharge efficiency

--- nodes/energy-battery-config/energy-battery-config.js
+++ nodes/energy-battery-config/energy-battery-config.js
@@ -5,7 +5,8 @@ module.exports = function(RED) {
     this.capacity_kwh = Number(n.capacity_kwh ?? 15)
     this.max_charge_power_kw = Number(n.max_charge_power_kw ?? 10)
     this.max_discharge_power_kw = Number(n.max_discharge_power_kw ?? 10)
-    this.charge_efficiency = Number(n.charge_efficiency ?? 10)
+    this.charge_efficiency = Number(n.charge_efficiency ?? 1)
+    this.discharge_efficiency = Number(n.discharge_efficiency ?? 1)
   }
   RED.nodes.registerType("energy-battery-config",EnergyBatteryConfigNode);
 }
