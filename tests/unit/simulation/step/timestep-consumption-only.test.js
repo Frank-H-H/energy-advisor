e1c4cc3e88a800d897f70bb29dc6c68affb2c8a4
DESKTOP-CFT3LCM\Frank <git.frank@huxol.one>
2026-08-29 22:27:33 +0200

    added test for consumption only

--- tests/unit/simulation/step/timestep-consumption-only.test.js
+++ tests/unit/simulation/step/timestep-consumption-only.test.js
@@ -6,13 +6,13 @@ import {
   makeComponents,
 } from '../../../helpers/simulation.js';
 
-describe('simulateTimestep - influence of production only', () => {
+describe('simulateTimestep - influence of consumption only', () => {
   describe('for partial current frame', () => {
     const timestep = makeTimestep(
       '2026-04-08T12:00:00.000Z',
       '2026-04-08T12:15:00.000Z',
       {
-        expectedProductionPower: 6, // kW
+        expectedConsumptionPower: 6, // kW
       }
     );
     it('batteryEnergyAtEnd changes', () => {
@@ -30,11 +30,11 @@ describe('simulateTimestep - influence of production only', () => {
       expect(
         nextState.batteryEnergyAtEnd,
         'expected energy change = 6 kW * (10/60)h = 1 kWh'
-      ).toBeCloseTo(21, 6);
+      ).toBeCloseTo(19);
     });
-    it('batteryEnergyAtEnd cannot exceed capacity (charge capped at capacity)', () => {
+    it('batteryEnergyAtEnd cannot go below 0', () => {
       const state = makeState('2026-04-08T12:05:00.000Z', {
-        batteryEnergyAtStart: 42.8,
+        batteryEnergyAtStart: 0.2,
       });
       const components = makeComponents({});
 
@@ -42,8 +42,8 @@ describe('simulateTimestep - influence of production only', () => {
 
       expect(
         nextState.batteryEnergyAtEnd,
-        'batteryEnergyAtEnd capped at capacity: 6 kW * (10/60)h = 1 kWh (larger than missing 0.2)'
-      ).toBeCloseTo(43, 6); // clamped to capacity
+        'batteryEnergyAtEnd capped at 0: 6 kW * (10/60)h = 1 kWh (larger than remaining 0.2)'
+      ).toBeCloseTo(0, 6); // clamped to 0
     });
   });
   describe('for full future frame', () => {
@@ -51,7 +51,7 @@ describe('simulateTimestep - influence of production only', () => {
       '2026-04-08T13:00:00.000Z',
       '2026-04-08T13:15:00.000Z',
       {
-        expectedProductionPower: 4, // kW
+        expectedConsumptionPower: 4, // kW
       }
     );
     it('batteryEnergyAtEnd changes', () => {
@@ -69,11 +69,11 @@ describe('simulateTimestep - influence of production only', () => {
       expect(
         nextState.batteryEnergyAtEnd,
         'expected energy change = 4 kW * (15/60)h = 1 kWh'
-      ).toBeCloseTo(21, 6);
+      ).toBeCloseTo(19, 6);
     });
-    it('batteryEnergyAtEnd cannot exceed capacity (charge capped at capacity)', () => {
+    it('batteryEnergyAtEnd cannot go below 0', () => {
       const state = makeState('2026-04-08T12:05:00.000Z', {
-        batteryEnergyAtStart: 42.8,
+        batteryEnergyAtStart: 0.2,
       });
       const components = makeComponents({});
 
@@ -81,8 +81,8 @@ describe('simulateTimestep - influence of production only', () => {
 
       expect(
         nextState.batteryEnergyAtEnd,
-        'batteryEnergyAtEnd capped at capacity: 6 kW * (15/60)h = 1 kWh (larger than missing 0.2)'
-      ).toBeCloseTo(43, 6); // clamped to capacity
+        'batteryEnergyAtEnd capped at capacity: 6 kW * (15/60)h = 1 kWh (larger than remaining 0.2)'
+      ).toBeCloseTo(0, 6); // clamped to 0
     });
   });
 });
