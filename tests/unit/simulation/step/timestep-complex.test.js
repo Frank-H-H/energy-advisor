8af9031b7ec2f93a9157ee62a16a5a771e49a868
Frank-H-H <github.frank@huxol.one>
2026-08-30 11:41:18 +0200

    added more complex test scenario

---
+++ tests/unit/simulation/step/timestep-complex.test.js
@@ -0,0 +1,62 @@
+// tests/unit/step-full.test.js
+import { describe, it, expect } from 'vitest';
+import { simulateTimestep } from '../../../../src/simulation/timestep.js';
+import {
+  defaultSimpleTestSettingsForPartialStepFixture,
+  defaultSimpleTestSettingsForFullStepFixture,
+  expectStandardNextStateAttributesPresent,
+} from '../../../helpers/simulation.js';
+
+describe('simulateTimestep - complex scenarios', () => {
+  describe('solar, consumption, extra load and grid target', () => {
+    it('for partial current frame', () => {
+      const testFixture = defaultSimpleTestSettingsForPartialStepFixture(
+        {},
+        {
+          expectedProductionPower: 6,
+          expectedConsumptionPower: 3,
+          gridTarget: 0.6,
+          extraConsumptionPower: 2.1,
+          extraConsumptionEndsAt: new Date('2026-04-08T14:00:00.000Z'),
+        }
+      );
+      const { nextState } = simulateTimestep(testFixture);
+
+      // 20        # battery start
+      // + 1       # production
+      // - 0.5     # consumption
+      // + 0.10    # target grid point of 10 minutes
+      // - 0.35     # car loading of 10 minutes
+      expect(
+        nextState.batteryEnergyAtEnd,
+        'expected energy change = 20 + 1 - 0.5 + 0.1 - 0.35 = '
+      ).toBeCloseTo(20.25);
+      expectStandardNextStateAttributesPresent(nextState);
+    });
+
+    it('for full future frame', () => {
+      const testFixture = defaultSimpleTestSettingsForFullStepFixture(
+        {},
+        {
+          expectedProductionPower: 6,
+          expectedConsumptionPower: 3,
+          gridTarget: 0.6,
+          extraConsumptionPower: 2.1,
+          extraConsumptionEndsAt: new Date('2026-04-08T14:00:00.000Z'),
+        }
+      );
+      const { nextState } = simulateTimestep(testFixture);
+
+      // 20        # battery start
+      // + 1.5     # production
+      // - 0.75    # consumption
+      // + 0.15    # grid point
+      // - 0.525     # extra consumption
+      expect(
+        nextState.batteryEnergyAtEnd,
+        'expected energy change = 20 + 1.5 - 0.75 + 0.15 - 0.525 = 20.375'
+      ).toBeCloseTo(20.375);
+      expectStandardNextStateAttributesPresent(nextState);
+    });
+  });
+});
